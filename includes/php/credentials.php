1b125062c57399cd3ebf57c35dc7ed82385993db
Kento Hyono <keeeeento4649@icloud.com>
2023-10-04 19:27:15 -0700

    Added customer and admin login functionality

--- includes/php/credentials.php
+++ includes/php/credentials.php
@@ -1,19 +1,42 @@
 <?php 
-    if (!empty($_POST['username']) && !empty($_POST['pass'])){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        if (!empty($_POST['username']) && !empty($_POST['pass'])){
+        
+            // pass form fields into variables
+            $username = $_POST['username'];
+            $password = $_POST['pass']; 
     
-        // pass form fields into variables
-        $username = $_POST['username'];
-        $password = $_POST['pass']; 
+            // search username and password pair in the database
+            $sql = "select user_id, username, password, role from users where username = '$username' and password = SHA1('$password');";
+            $result = mysqli_query($dbc,$sql);
+            $num = mysqli_num_rows($result); // Count the number of row.
+    
+            // error message variable
+            $creds = "";
+    
+            // if there is a returned row
+            if ($num == 1){
+                $row = mysqli_fetch_array($result);
+                $role = $row["role"];
 
-        // error message variable
-        $creds = "";
-
-        if($username == "Manny" && $password == "12345"){
-            header("Location: ./admin.php");  // go to admin page if credentials are correct
-            exit;
-        } else {
-            $creds = 'Invalid Credentials';
-            print '<p style="color: red;">' .$creds. '</p>';    // print error message underneath form if credentials are wrong
+                // TODO: Session thing is not working
+                // $_SESSION("user_id") = $row["user_id"]; // store user_id to the session
+                // $_SESSION("user_name") = $row["username"]; // store username to the session
+                
+                // go to admin or customer page depending on a returned role
+                if ($role == 'A') {
+                    header("Location: ../../admin.php");  // go to admin page 
+                } else {
+                    header("Location: ../../index.php"); // go to customer page
+                }
+    
+                exit;
+            } else {
+                $creds = 'Invalid Credentials';
+                print '<p style="color: red;">' .$creds. '</p>';    // print error message underneath form if credentials are wrong
+            }
+            
         }
         
     }
