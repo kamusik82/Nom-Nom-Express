568e2a719ad1e17cd8761a9756fb8239f63326d0
Kento Hyono <keeeeento4649@icloud.com>
2023-10-07 15:03:38 -0700

    Added edit functionality almost working

--- admin.php
+++ admin.php
@@ -7,17 +7,18 @@
 </head>
 
 <body>
-	
-	<?php include ("./includes/php/connection.php"); ?> <!-- include for connection php to connect to the database -->
 
-	<form action='./includes/php/logout.php' method='post'>	<!-- logout button form action takes you to the logout page (will probably update to just take them back to the landing page) -->
+	<?php include("./includes/php/connection.php"); ?> <!-- include for connection php to connect to the database -->
+
+	<form action='./includes/php/logout.php' method='post'>
+		<!-- logout button form action takes you to the logout page (will probably update to just take them back to the landing page) -->
 		<input type='hidden' name='logout' value='true' />
 		<input type='submit' value='Logout' /> <!-- logout submit button -->
 	</form>
 
 	<a href="./index.php"><button>Index</button></a>
 
-	<?php include ("./includes/php/store_info.php"); ?> <!-- include for the store_info -->
+	<?php include("./includes/php/store_info.php"); ?> <!-- include for the store_info -->
 
 	<h1> Store info</h1>
 	<!-- information that comes from store_info php -->
@@ -64,7 +65,8 @@
 
 	<h1>Add Menu Items Form</h1>
 
-	<?php include ("./includes/php/add.php"); ?> <!-- include add that validates given form data and updates the database -->
+	<?php include("./includes/php/add.php"); ?>
+	<!-- include add that validates given form data and updates the database -->
 
 	<form id="addItem" enctype="multipart/form-data" action="admin.php" method="POST">
 		<!-- form action is the admin page to stay on the same page -->
@@ -120,7 +122,8 @@
 
 	<h1>Current Menu Items</h1>
 
-	<?php include ("./includes/php/items.php"); ?> <!-- display current items with php include --> 
+	<?php include("./includes/php/items.php"); ?> <!-- display current items with php include -->
+	<?php include("./includes/php/edit.php"); ?> <!-- edit current items with php include -->
 
 	<!-- Confirmation Modal for delete, disable, enable-->
 	<div class="modal fade" id="confirmationModal" tabindex="-1" aria-labelledby="confirmationLabel" aria-hidden="true">
@@ -136,17 +139,52 @@
 					<h6 class="text-end confirmation-bottom"></h6>
 				</div>
 				<div class="modal-footer">
-					<a class="btn btn-secondary" href="#" role="button" data-bs-dismiss="modal">No</a> <!-- Just close modal -->
-				    <a class="btn btn-danger" href="admin.php" role="button">Yes</a> <!-- the url of this confirmation button will change depending on the button clicked -->
+					<a class="btn btn-secondary" href="#" role="button" data-bs-dismiss="modal">No</a>
+					<!-- Just close modal -->
+					<a class="btn btn-danger" href="admin.php" role="button">Yes</a>
+					<!-- the url of this confirmation button will change depending on the button clicked -->
 				</div>
 			</div>
 		</div>
 	</div>
 
-	<form action='./includes/php/logout.php' method='post'>	<!-- logout button form action takes you to the logout page (will probably update to just take them back to the landing page) -->
-		<input type='hidden' name='logout' value='true' />
-		<input type='submit' value='Logout' /> <!-- logout submit button -->
-	</form>
+	<!-- Edit item information modal -->
+	<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h1 class="modal-title fs-5" id="editLabel">Edit Item Information</h1>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<form action="admin.php" method="POST">
+						<p>
+							Item Name :
+							<input type="text" name="newName" value="">
+							<!-- keeps the item name value given and also protects against injection attacks -->
+							<?php print $newNameErr; ?> <!-- error message if name is empty from add.php -->
+						</p>
+						<div class="d-flex flex-columns my-4">
+							<label for="newDescArea">Item Description : </label>
+
+							<textarea id="newDescArea" rows="5" cols="30" type="text" name="newDesc" value=""></textarea>
+							<!-- keeps the item name value given and also protects against injection attacks -->
+							<?php print $newDescErr; ?> <!-- error message if description is empty from add.php -->
+						</div>
+
+						<p>
+							Item Price :
+							<input type="text" name="newPrice" value="">
+							<!-- keeps the item name value given and also protects against injection attacks -->
+							<?php print $newPriceErr; ?> <!-- error message if price is empty from add.php -->
+						</p>
+						<input type="hidden" name="itemID" value="">
+						<input type="submit" name="edit" value="SUBMIT">
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<script src="https://code.jquery.com/jquery-3.7.0.min.js"
 		integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
@@ -198,6 +236,22 @@
 			$("#confirmationModal .modal-footer .btn-danger").attr('href', `./admin.php?action=enable&itemID=${item_id}`);
 
 			$('#confirmationModal').modal('show');
+		})
+
+		// When clicked edit button
+		$(document).on('click', '.edit', function () {
+
+			let item_id = $(this).attr("item_id");
+			let item_name = $(this).attr("item_name");
+			let item_desc = $(this).attr("item_desc");
+			let item_price = $(this).attr("item_price");
+
+			$("#editModal input[name=itemID]").val(item_id);
+			$("#editModal input[name=newName]").val(item_name);
+			$("#editModal textarea[name=newDesc]").val(item_desc);
+			$("#editModal input[name=newPrice]").val(item_price);
+
+			$("#editModal").modal('show');
 		})
 	</script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
