560836460de01011a397fdfe7db87015df61f1f1
Manny-Sandhu <manindersandhu@hotmail.ca>
2023-10-12 15:20:31 -0700

    Login Modal only closes after correct information is added and retryinng to image path for cart

--- index.php
+++ index.php
@@ -30,9 +30,23 @@
         </div>
     </div>
 
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+		integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"></script>
+
+        <!-- this script makes it so the modal keeps opening unless the correct information is put in -->
+        <?php 
+            echo "<script>$(document).ready(function(){";
+            if (!empty($userErr) || !empty($passErr) || !empty($creds)) {
+                echo '$("#loginModal").modal("show");';
+            } else {
+                echo '$("#loginModal").modal("hide");';
+            }
+            echo "});</script>";
+        ?>
 </body>
 
 </html>
