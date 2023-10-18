e9d46a453c1496388e50e7052687c39d2a3af7e6
Kento Hyono <keeeeento4649@icloud.com>
2023-10-17 17:21:22 -0700

    Combined checkout and create confirmation file functionalities

--- includes/php/cart_to_order.php
+++ includes/php/cart_to_order.php
@@ -23,13 +23,14 @@
                 $result_order = @mysqli_query($dbc, $sql_order);
 
                 // get the order number just created upon insert into orders table
-                $sql_order_num = "SELECT max(order_id) FROM orders;";
+                $sql_order_num = "SELECT max(order_id), date FROM orders;";
                 $result_order_num = @mysqli_query($dbc, $sql_order_num);
                 $row_order_num = mysqli_fetch_assoc($result_order_num);
+                $date = $row_order_num['date'];
                 $order_num = $row_order_num['max(order_id)'];
 
                 // get rows from cart_items
-                $sql_cart = "SELECT cart_items.item_id, quantity, item_price, item_name FROM cart_items INNER JOIN menu_items USING (item_id) WHERE user_id = $u_id;";
+                $sql_cart = "SELECT cart_items.item_id, quantity, item_price, item_picture, item_name FROM cart_items INNER JOIN menu_items USING (item_id) WHERE user_id = $u_id;";
                 $result_cart = @mysqli_query($dbc, $sql_cart);
                 $num_cart = mysqli_num_rows($result_cart);
 
@@ -37,6 +38,7 @@
                 $priceArray = array();
                 $nameArray = array();
                 $quantityArray = array();
+                $pictureArray = array();
 
                 // for each cart item, insert into order_items table and delete from cart_items table
                 while ($row_cart = mysqli_fetch_array($result_cart, MYSQLI_ASSOC)) {
@@ -44,11 +46,13 @@
                     $itemName = $row_cart['item_name'];
                     $price = $row_cart['item_price'];
                     $quant = $row_cart['quantity'];
+                    $picture = $row_cart['item_picture'];
 
                     // gather item info for confirmation message               
                     array_push($priceArray, $price);
                     array_push($nameArray, $itemName);
                     array_push($quantityArray, $quant);
+                    array_push($pictureArray, $picture);
 
                     // Insert into order_items table
                     $sql_items = "INSERT INTO order_items (order_id, item_id, cost, quantity) VALUES ($order_num, $item, $price, $quant);";
@@ -60,16 +64,19 @@
                 }
                 
                 // get users name to use in thank you message
-                $sql_user = "SELECT first_name FROM users WHERE user_id=$u_id;";
+                $sql_user = "SELECT first_name, last_name FROM users WHERE user_id=$u_id;";
                 $result_user = @mysqli_query($dbc, $sql_user);
                 $row_user = mysqli_fetch_assoc($result_user);
-                $user = $row_user['first_name'];
+                $first_name = $row_user['first_name'];
+                $last_name = $row_user['last_name'];
+
+                include('./orderConf.php');
             ?>
 
         </div>
         <div class="container">
             <div class="row mb-3">
-                <h5>Thank you for your Nom Nom Express order <span class="fw-bold"><?php print $user?></span>!</h5>
+                <h5>Thank you for your Nom Nom Express order <span class="fw-bold"><?php print $first_name?></span>!</h5>
                 <br>
             </div>
             <div class="row mb-2">
--- includes/php/orderConf.php
+++ includes/php/orderConf.php
@@ -1,51 +1,5 @@
 <?php # This file creates the order confirmation information stored in each order
 
-# TODO: Include connection.php. This will be removed later
-include('connection.php');
-
-# TODO: Put dummy order_id which is necessary for retaining order information detail in for now 
-$order_id = 3;
-
-# Build and Run the query
-$query = "SELECT order_id, first_name, last_name, email, total, date, shipping, tax, item_name, item_price, quantity, item_picture
-          FROM orders INNER JOIN order_items USING (order_id) 
-                      INNER JOIN menu_items USING (item_id)
-                      INNER JOIN users USING (user_id)
-          WHERE order_id = $order_id;";
-$result = @mysqli_query($dbc, $query);
-
-if ($result) {
-    // Initiate variables 
-    $first_name = $last_name = $email = $total = $date = $shipping = $tax;
-
-    // create arrays for each column in the query
-    $priceArray = array();
-    $nameArray = array();
-    $quantityArray = array();
-    $pictureArray = array();
-
-    $rowCount = 0;
-
-    while ($itemRow = @mysqli_fetch_array($result)) {
-
-        if ($rowCount == 0) { // Assign values just once
-            $first_name = $itemRow['first_name'];
-            $last_name = $itemRow['last_name'];
-            $email = $itemRow['email'];
-            $total = $itemRow['total'];
-            $date = $itemRow['date'];
-            $shipping = $itemRow['shipping'];
-            $tax = $itemRow['tax'];
-        }
-
-        array_push($priceArray, $itemRow['item_price']);
-        array_push($nameArray, $itemRow['item_name']);
-        array_push($quantityArray, $itemRow['quantity']);
-        array_push($pictureArray, $itemRow['item_picture']);
-
-        $rowCount++;
-    }
-
     # Order Confirmation File
     ob_start(); ?>
 
@@ -57,18 +11,12 @@ if ($result) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <link rel="stylesheet" href="../style.css">
         <title>Order Confirmation</title>
     </head>
     <style>
         body {
             max-width: 800px;
-        }
-
-        .item-pic {
-            max-width: 220px;
-            width: 100%;
-            aspect-ratio: 1/1; 
-            object-fit: cover;
         }
     </style>
 
@@ -76,7 +24,7 @@ if ($result) {
         <h2>Order Confirmation</h2>
         <p>Hi <span class="fw-bold"></span><?php echo "$first_name $last_name" ?></span></p>
         <p>Thank you for shopping at Nom Nom Express.<br>
-            Your order id No.<?php echo $order_id ?> is confirmed. <br>
+            Your order id No.<?php echo $order_num ?> is confirmed. <br>
             We'll let you know when your order ships.
         </p>
         
@@ -91,7 +39,7 @@ if ($result) {
 
                         for ($i = 0; $i < count($nameArray); $i++) {
                             print   "<tr>
-                                        <td align='left'><img src='../images/$pictureArray[$i]' class='item-pic'></td>
+                                        <td align='left'><img src='../images/$pictureArray[$i]' class='cart_picture'></td>
                                         <td align='left' class='align-middle'>
                                             <div >
                                                 <h5 class='my-3'>$nameArray[$i]</h5>
@@ -110,19 +58,23 @@ if ($result) {
                 <tbody class="me-3">
                     <tr>
                         <td class="text-secondary">Order ID</td>
-                        <td class="text-secondary" align="right">No.<?php echo $order_id ?></td>
+                        <td class="text-secondary" align="right">No.<?php echo $order_num ?></td>
+                    </tr>
+                    <tr>
+                        <td class="text-secondary">Sub-total</td>
+                        <td class="text-secondary" align="right">$<?php print number_format(($totalamt-$taxamt-$shipamt), 2)?></td>
                     </tr>
                     <tr>
                         <td class="text-secondary" >Tax</td>
-                        <td class="text-secondary" align="right">$<?php echo $tax ?></td>
+                        <td class="text-secondary" align="right">$<?php echo number_format($taxamt, 2) ?></td>
                     </tr>
                     <tr>
                         <td class="text-secondary">Shipping</td>
-                        <td class="text-secondary" align="right">$<?php echo $shipping ?></td>
+                        <td class="text-secondary" align="right">$<?php echo number_format($shipamt, 2) ?></td>
                     </tr>
                     <tr>
                         <td class="fw-bold">Total</td>
-                        <td class="fw-bold" align="right">$<?php echo $total ?></td>
+                        <td class="fw-bold" align="right">$<?php echo number_format($totalamt, 2) ?></td>
                     </tr>
                 </tbody>
             </table>
@@ -137,7 +89,7 @@ if ($result) {
     <!-- html end -->
 
     <?php $orderConf = ob_get_clean(); # HTML is stored in orderConf variable
-        $fileName = "O$order_id.php";
+        $fileName = "O$order_num.php";
 
         // echo $orderConf;
 
@@ -146,6 +98,5 @@ if ($result) {
 
     # Create order confirmation file - Be sure to have permission or chmod
     file_put_contents($fileName, $orderConf, FILE_APPEND | LOCK_EX);
-}
 
 ?>
