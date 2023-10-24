72f78614cb9b45347fab149a403609749a41b6c8
Kamila <kamila.diyanova@gmail.com>
2023-10-24 10:12:50 -0700

    Updating final report (skills we learned, proposed changes, cose snipets)

---
+++ includes/orders/O94.php
@@ -0,0 +1,92 @@
+
+    <!-- Actual html content here -->
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/style.css">
+        <title>Nom Nom Express</title>
+    </head>
+
+    <body class="m-5">
+        <div class="container background">
+            <h2 class="pt-5">Order Confirmation</h2>
+            <p>Hi <span class="fw-bold"></span>Bob Baker</span></p>
+            <p>Thank you for shopping at Nom Nom Express.<br>
+                Your order id No.94 is confirmed. <br>
+                We'll let you know when your order ships.
+            </p>
+            
+            <h3>Order details</h3>
+            <p>Placed on
+                2023-10-07 21:15:58            </p>
+            <div class="border-top border-bottom my-3">
+                <table class="table table-borderless border-bottom">
+                    <tbody>
+                        <tr>
+                                            <td align='left'><img src='../images/set1.jpg' class='cart_picture'></td>
+                                            <td align='left' class='align-middle'>
+                                                <div >
+                                                    <h5 class='my-3 colour'>Hearty Breakfast Delight</h5>
+                                                    <p class='fw-bold colour'>$20.50 <span class='colour'>x 1</span></p>
+                                                </div>
+                                            </td>
+                                        </tr><tr>
+                                            <td align='left'><img src='../images/veggiburger.jpg' class='cart_picture'></td>
+                                            <td align='left' class='align-middle'>
+                                                <div >
+                                                    <h5 class='my-3 colour'>Delicious Veggie Burger</h5>
+                                                    <p class='fw-bold colour'>$18.90 <span class='colour'>x 1</span></p>
+                                                </div>
+                                            </td>
+                                        </tr><tr>
+                                            <td align='left'><img src='../images/brownie.jpg' class='cart_picture'></td>
+                                            <td align='left' class='align-middle'>
+                                                <div >
+                                                    <h5 class='my-3 colour'>Brownie Bliss with Cream</h5>
+                                                    <p class='fw-bold colour'>$15.50 <span class='colour'>x 1</span></p>
+                                                </div>
+                                            </td>
+                                        </tr>                    </tbody>
+                </table>
+            </div>
+            <div class="border-bottom">
+                <table class="table table-borderless border-bottom colour">
+                    <tbody class="me-3">
+                        <tr>
+                            <td>Order ID</td>
+                            <td align="right">No.94</td>
+                        </tr>
+                        <tr>
+                            <td>Sub-total</td>
+                            <td align="right">$54.90</td>
+
+                        <tr>
+                            <td>Tax</td>
+                            <td align="right">$7.49</td>
+                        </tr>
+                        <tr>
+                            <td>Shipping</td>
+                            <td align="right">$7.50</td>
+                        </tr>
+                        <tr>
+                            <td class="fw-bold">Total</td>
+                            <td class="fw-bold" align="right">$69.89</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+            crossorigin="anonymous"></script>
+    </body>
+
+    </html>
+    <!-- html end -->
+
+    
